f2d4115cdffcc4c9c0a4271c00af1ca8381af627
Jordan Wells <jordantwells@gmail.com>
2022-07-18 19:13:30 -0400

    better spotify background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,7 +119,7 @@ const Home: NextPage = () => {
     <div className='bg-slate-700 text-white flex flex-col items-center justify-center text-4xl w-full h-screen'>
       <p className='w-5/6 text-center'>Sign in with Spotify to use <i>Gradiance</i> </p>
       <button
-        className='bg-green-700 p-4 w-3/4 md:w-1/3 h-24 flex flex-row items-center justify-center border-white rounded-2xl m-10'
+        className='bg-emerald-700 p-4 w-3/4 md:w-1/3 h-24 flex flex-row items-center justify-center border-white rounded-2xl m-10'
         onClick={() => signIn('spotify')}
       >
         <img className='h-full' src='/spotify_logo.png'></img>
