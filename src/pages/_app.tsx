3b40156cd26988c79afe4a56d8d69d3b5ba0bdac
Jordan Wells <jordantwells@gmail.com>
2022-07-18 00:39:48 -0400

    bare minimum seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider  session={session}>
+      <Head>
+        <title>Gradiance</title>
+      </Head>
       <Component  {...pageProps} />
     </SessionProvider>
   );
