4060dd1604b3f2f1e615235963fa2fcc8c38d40b
Jordan Wells <jordantwells@gmail.com>
2022-07-21 08:27:31 -0400

    added spotify logo to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,10 @@
 export default function Footer(){
     return (
         <div className="w-full h-10 flex justify-between items-center p-5 bg-[#222] text-white text-base absolute bottom-0">
-            <h1>Made by <a className="underline" href="https://jordantwells.com">Jordan Wells</a></h1>
+            <div className="flex w-full gap-4">
+            <h1 >Made by <a className="underline" href="https://jordantwells.com">Jordan Wells</a></h1>
+            <h1 className="flex">Powered by the <a href="https://spotify.com"><img className="w-5 mx-2 aspect-square object-contain" alt="Spotify Logo" src="/spotify.png" /></a> Spotify API</h1>
+            </div>
         <a href="https://github.com/jordantwells42">
         <img
           className="h-8 brightness-200 invert"
