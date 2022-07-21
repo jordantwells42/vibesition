a7baba0584bace2272501d74ec562e91146c0476
Jordan Wells <jordantwells@gmail.com>
2022-07-21 09:32:00 -0400

    changing footer sizing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
-export default function Footer(){
-    return (
-        <div className="w-full h-10 flex justify-between items-center p-5 bg-[#222] text-white text-base absolute bottom-0">
-            <div className="flex w-full gap-4">
-            <h1 >Made by <a className="underline" href="https://jordantwells.com">Jordan Wells</a></h1>
-            <h1 className="flex">Powered by the <a href="https://spotify.com"><img className="w-5 mx-2 aspect-square object-contain" alt="Spotify Logo" src="/spotify.png" /></a> Spotify API</h1>
-            </div>
-        <a href="https://github.com/jordantwells42">
+export default function Footer() {
+  return (
+    <div className="absolute bottom-0 flex h-10 w-full items-center justify-between bg-[#222] p-5 text-white text-xs md:text-base">
+      <div className="flex w-full gap-4">
+        <h1>
+          Made by{" "}
+          <a className="underline" href="https://jordantwells.com">
+            Jordan Wells
+          </a>
+        </h1>
+        <h1 className="flex">
+          Powered by the{" "}
+          <a href="https://spotify.com">
+            <img
+              className="mx-2 aspect-square w-5 object-contain"
+              alt="Spotify Logo"
+              src="/spotify.png"
+            />
+          </a>{" "}
+          Spotify API
+        </h1>
+      </div>
+      <a href="https://github.com/jordantwells42">
         <img
           className="h-8 brightness-200 invert"
           src="/github.png"
-          
           aria-label="Follow @jordantwells42 on GitHub"
         />
-        </a>
-      </div>
-    )
+      </a>
+    </div>
+  );
 }
